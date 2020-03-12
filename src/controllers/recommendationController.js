e73956db0bd55bac5f8edb2b4d0c3cd6a989b37b
danielavellar15 <danielavellar15@hotmail.com>
2020-03-12 11:11:42 -0300

    separacao por tipo de recomendacao

--- src/controllers/recommendationController.js
+++ src/controllers/recommendationController.js
@@ -8,15 +8,30 @@ exports.get = (req, res, next) => {
     res.status(200).send(products);
 };
 
-exports.getProduct = async (req, res, next) => {
-    const products = await Product.find()
-    res.json(products);
-};
-
 exports.post = (req, res, next) => {
     res.status(201).send('Requisição recebida com sucesso!');
 };
 
-exports.getById = (req, res, next) => {
-    res.status(200).send('Requisição recebida com sucesso!');
+exports.getProduct = async (req, res, next) => {
+    const { filters, type } = req.body        
+    let products;
+
+    console.log(type);
+
+    if( type == 'BY_USER') {
+        products = await recommendationByUser();
+    } 
+    else if (type == 'GLOBAL') {
+        products = await recommendationGlobal();
+    }
+    
+    res.json(products);
 };
+
+async function recommendationByUser() {
+    return await Product.find().sort({price: 'asc'})
+}
+
+async function recommendationGlobal() {
+    return await Product.find().sort({price: 'desc'})
+}
--- src/routes/recommendationRoute.js
+++ src/routes/recommendationRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/recommendationController');
+
 router.post('/', controller.post);
 router.get('/', controller.get);
 router.get('/products', controller.getProduct);
-router.get('/:id', controller.getById);
+
 module.exports = router;
